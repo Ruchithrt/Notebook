68d5fb08117d286f76c4c373fe85c5566afb8448
Ruchithrt <ruchithtilavalli3266@gmail.com>
2024-02-07 11:36:28 +0530

    final

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017";
+const mongoose = require('mongoose');
 
-async function connectToMongo() {
-  await mongoose
-    .connect(mongoURI)
-    .then(() => console.log("Connected to Mongo Successfully"))
-    .catch((err) => console.log(err));
+const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log("Connected to Mongo Successfully");
+    })
 }
 
 module.exports = connectToMongo;
--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,19 @@
-const connectToMongo = require("./db");
+const connectToMongo = require('./db');
+const express = require('express')
+var cors = require('cors') 
+
 connectToMongo();
-const express = require("express");
+const app = express()
+const port = 5000
 
-const app = express();
-const port = 3000;
+app.use(cors())
+app.use(express.json())
 
-app.use(express.json());
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
-//available routes
-app.get("/api/auth", require("./routes/auth"));
-app.get("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`iNotebook backend listening at http://localhost:${port}`)
+})
